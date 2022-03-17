418327ace99fe351a4fd620e80550fab4dcb41f9
Lapulatos <2627231235@qq.com>
2022-03-17 09:45:00 +0800

    config and test.c

--- test.c
+++ test.c
@@ -1,18 +1,21 @@
 typedef unsigned long pthread_t;
 
+extern void reach_error();
 
-int x = 0;
+int x = -1, y = 2;
 
 void t1(void *arg)
 {
-	int l1 = x + 1;
-	x = l1 + x;
+	y = 1;
+	x = x + 1;
 }
 
 void t2(void *arg)
 {
-	int l2 = x + 2;
-	x = l2 + x;
+	x = x + 1;
+	if(x <= y - 2) {
+		y = x + 1;
+	}
 }
 
 int main()
@@ -21,9 +24,13 @@ int main()
 	
 	pthread_create(&tid1, 0, t1, 0);
 	pthread_create(&tid2, 0, t2, 0);
-	
+
 	pthread_join(tid1, 0);
 	pthread_join(tid2, 0);
+	
+	if(!(y < 2)) {
+		reach_error();
+	}
 	
 	return 0;
 }
